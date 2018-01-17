0aa3f6d7345a3e507a23fcdeb6efcf72aa066333
Alex Wait <alex@chirpify.com>
2018-01-17 12:05:03 -0800

    Changing config to serve the right index.html file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DayPicker from './components/day_picker.jsx';
-
+console.log("running main.jsx!");
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
     React.createElement(DayPicker),
