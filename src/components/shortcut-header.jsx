18532d5f09feae24fbe03cb14ff8507156e11176
Jing Wait <jingzhaowait@gmail.com>
2018-01-19 10:39:06 -0800

    install classnames package, and predefined hours

--- src/components/shortcut-header.jsx
+++ src/components/shortcut-header.jsx
@@ -1,23 +1,62 @@
 import React from 'react';
 import '../styles/components/shortcut-header.scss';
+import moment from 'moment';
+import classNames from 'classnames'
 
 class ShortcutHeader extends React.PureComponent {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.toggleClass = this.toggleClass.bind(this);
     this.state = {
+      startDate: moment().subtract(24, 'hours').format(),
+      endDate: moment().format(),
     };
+    this.state.predefinedHours = [
+      {
+        hours: 1,
+        name: "1h",
+        active: false
+      },
+      {
+        hours: 3,
+        name: "3h",
+        active: false
+      },
+      {
+        hours: 12,
+        name: "12h",
+        active: false
+      },
+      {
+        hours: 24,
+        name: "1d",
+        active: true
+      },
+      {
+        hours: 72,
+        name: "3d",
+        active: false
+      },
+      {
+        hours: 168,
+        name: "1w",
+        active: false
+      },
+    ]
   }
 
+  toggleClass
+
   render() {
+
+    var btnClass = classNames({
+      'button': true,
+      'active': this.state.predefinedHour.active
+    })
     return (
       <div>
-        <span>1h</span>
-        <span>3h</span>
-        <span>12h</span>
-        <span>1d</span>
-        <span>3d</span>
-        <span>1w</span>
-        <span>custom</span>
+        <div>{this.state.endDate} -- {this.state.startDate}</div>
+        <span className={btnClass} onClick={this.toggleClass}>custom</span>
       </div>
 
     );
