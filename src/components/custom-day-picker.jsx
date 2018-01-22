9b9d134f131ca6acbcd489b4b4ce635a64c0e068
Jing Wait <jingzhaowait@gmail.com>
2018-01-22 14:17:30 -0800

    toggle absolute and relative day picker class

--- src/components/custom-day-picker.jsx
+++ src/components/custom-day-picker.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import '../styles/components/shortcut-header.scss';
+import '../styles/components/custom-day-picker.scss';
 
 class CustomDayPicker extends React.PureComponent {
 
   constructor(props) {
     super(props);
+    this.state = {
+      absolute: true,
+    }
 
+  }
+
+  toggleClass(v) {
+    if(v == 'absolute') {
+      this.setState({
+        absolute: true
+      })
+    }else {
+      this.setState({
+        absolute: false
+      })
+    }
   }
 
   render() {
     return (
-      <div>
-        <div><span>Absolute</span><span>Relative</span></div>
+      <div className={'dayPickerContainer'}>
+        <div>
+          <span className={'button ' + (this.state.absolute? 'btnActive' : '')} onClick={this.toggleClass.bind(this, 'absolute')}>Absolute</span>
+          <span className={'button ' + (this.state.absolute? '' : 'btnActive')} onClick={this.toggleClass.bind(this, 'relative')}>Relative</span>
+        </div>
 
 
       </div>
