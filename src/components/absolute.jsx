2976f2052d95806084e538bec4a7a997dc79c7cf
Jing Wait <jingzhaowait@gmail.com>
2018-01-23 13:24:18 -0800

    create absolute component

---
+++ src/components/absolute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Absolute extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    <div></div>
+  }
+}
+
+export default Absolute
